98646fe6eacc4944f43426ad9df0c01468a8bf01
gabrielpda15 <gabriel.pda15@gmail.com>
2024-06-13 15:03:10 -0300

    fix: added order field to database

---
+++ src/app/services/database/migrations/1718301332466-trackOrder.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class TrackOrder1718301332466 implements MigrationInterface {
+    name = 'TrackOrder1718301332466'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`track\` ADD \`order\` int NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`track\` DROP COLUMN \`order\``);
+    }
+
+}
